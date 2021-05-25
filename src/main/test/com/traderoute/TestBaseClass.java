00e475449587e500fad3c8f9de9d3c3fe669675c
Alex Alber <alex.alber.aa@gmail.com>
2021-05-25 12:33:59 +0200

    Created a base test class which contains methods to test Tableviews

---
+++ src/main/test/com/traderoute/TestBaseClass.java
@@ -0,0 +1,61 @@
+package com.traderoute;
+
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.loadui.testfx.exceptions.NoNodesFoundException;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+
+@ExtendWith(ApplicationExtension.class)
+public class TestBaseClass {
+
+    /**
+     * IMPORTED FROM TESTFX LOADUI, BUT NEEDED TO COPY METHODS IN ORDER
+     * to access them.
+     */
+
+    static TableView<?> getTableView(String tableSelector, FxRobot robot) {
+        Node node = robot.lookup(tableSelector).queryTableView();
+        if (!(node instanceof TableView)) {
+            throw new NoNodesFoundException(tableSelector + " selected " + node + " which is not a TableView!");
+        } else {
+            return (TableView)node;
+        }
+    }
+
+    protected static TableRow<?> row(String tableSelector, int row, FxRobot robot) {
+        TableView<?> tableView = getTableView(tableSelector, robot);
+
+        ObservableList current;
+        for(current = tableView.getChildrenUnmodifiable(); current.size() == 1; current = ((Parent)current.get(0)).getChildrenUnmodifiable()) {
+        }
+
+        for(current = ((Parent)current.get(1)).getChildrenUnmodifiable(); !(current.get(0) instanceof TableRow); current = ((Parent)current.get(0)).getChildrenUnmodifiable()) {
+        }
+
+        Node node = (Node)current.get(row);
+        if (node instanceof TableRow) {
+            return (TableRow)node;
+        } else {
+            throw new RuntimeException("Expected Group with only TableRows as children");
+        }
+    }
+
+    protected static TableCell<?, ?> cell(String tableSelector, int row, int column, FxRobot robot) {
+        ObservableList current;
+        for(current = row(tableSelector, row, robot).getChildrenUnmodifiable(); current.size() == 1 && !(current.get(0) instanceof TableCell); current = ((Parent)current.get(0)).getChildrenUnmodifiable()) {
+        }
+
+        Node node = (Node)current.get(column);
+        if (node instanceof TableCell) {
+            return (TableCell)node;
+        } else {
+            throw new RuntimeException("Expected TableRowSkin with only TableCells as children");
+        }
+    }
+}
