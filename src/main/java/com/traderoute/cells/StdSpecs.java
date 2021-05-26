0e00c955a3fef95b4e2850dcf08ee4c9094a771c
Alex Alber <alex.alber.aa@gmail.com>
2021-05-27 00:35:48 +0200

    Added enum for common CellSpecs called StdSpecs

---
+++ src/main/java/com/traderoute/cells/StdSpecs.java
@@ -0,0 +1,30 @@
+package com.traderoute.cells;
+
+import java.math.BigDecimal;
+
+public enum StdSpecs {
+
+    INTPOS5$(new CellSpecsBuilder().pre("$").post("").defaultValue(0).minValue(0).maxValue(10000).build()),
+    INTPOS6$(new CellSpecsBuilder().pre("$").post("").defaultValue(0).minValue(0).maxValue(100000).build()),
+    DECPOS5$(new CellSpecsBuilder().pre("$").post("").defaultValue(new BigDecimal("0.0"))
+            .minValue(new BigDecimal("0.0")).maxValue(new BigDecimal("10000")).build()),
+    DECPOS6$(new CellSpecsBuilder().pre("$").post("").defaultValue(new BigDecimal("0.0"))
+            .minValue(new BigDecimal("0.0")).maxValue(new BigDecimal("100000")).build()),
+    PERCENT(new CellSpecsBuilder().pre("$").post("").defaultValue(new BigDecimal("0.0"))
+            .minValue(new BigDecimal("0.0")).maxValue(new BigDecimal("99")).build());
+
+
+    private CellSpecs specs;
+
+//    private CrunchifyEnumCompany(String s) {
+//        companyLetter = s;
+//    }
+
+    private StdSpecs(CellSpecs specifications) {
+        specs = specifications;
+    }
+
+    public CellSpecs getSpecs() {
+        return specs;
+    }
+}
