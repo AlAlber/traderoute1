9c17ccf56f88e09a398a306897b81e4ac2465227
Alex Alber <alex.alber.aa@gmail.com>
2021-06-09 00:56:13 +0200

    Added another spec to the standard specs

--- src/main/java/com/traderoute/cells/StdSpecs.java
+++ src/main/java/com/traderoute/cells/StdSpecs.java
@@ -6,6 +6,8 @@ public enum StdSpecs {
     INTPOS5X(new CellSpecsBuilder().pre("").post("").defaultValue(0).minValue(0).maxValue(10000).build()),
     INTPOS5$(new CellSpecsBuilder().pre("$").post("").defaultValue(0).minValue(0).maxValue(10000).build()),
     INTPOS6$(new CellSpecsBuilder().pre("$").post("").defaultValue(0).minValue(0).maxValue(100000).build()),
+    DECPOS5X(new CellSpecsBuilder().pre("").post("").defaultValue(new BigDecimal("0.0"))
+            .minValue(new BigDecimal("0.0")).maxValue(new BigDecimal("10000")).build()),
     DECPOS5$(new CellSpecsBuilder().pre("$").post("").defaultValue(new BigDecimal("0.0"))
             .minValue(new BigDecimal("0.0")).maxValue(new BigDecimal("10000")).build()),
     DECPOS6$(new CellSpecsBuilder().pre("$").post("").defaultValue(new BigDecimal("0.0"))
